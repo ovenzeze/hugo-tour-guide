9a291d7750abb674dfd40411c0577ad3dbb26091
ZHEN <ovenzeze@qq.com>
2025-04-17 16:08:02 -0700

    Add favicon, robots.txt, and TypeScript configuration files
    - Added favicon.ico to the public directory.
    - Created robots.txt to allow all user agents.
    - Added server/tsconfig.json extending from Nuxt's server configuration.
    - Created tsconfig.json extending from Nuxt's main TypeScript configuration.

---
+++ nuxt.config.ts
@@ -0,0 +1,21 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+import { defineNuxtConfig } from 'nuxt/config'
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+
+  modules: ['shadcn-nuxt'],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
+
+})
